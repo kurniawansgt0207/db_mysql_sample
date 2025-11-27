50c74d45f27717b5d6eb06a97b7b32b80eca9296
kurniawansgt0207 <kurniawan.sgt@gmail.com>
2025-11-27 12:00:37 +0700

    upload file sql table struktur

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS `barang`;
+
+CREATE TABLE `barang` (
+  `kode_barang` varchar(3) NOT NULL,
+  `nama_barang` varchar(20) DEFAULT NULL,
+  `harga_barang` double(10,2) DEFAULT NULL,
+  `stok_barang` int(3) DEFAULT NULL,
+  PRIMARY KEY (`kode_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+/*Data for the table `barang` */
+
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A01','Barang A01',10000.00,10);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A02','Barang A02',20000.00,10);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A03','Barang A03',12000.00,20);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A04','Barang A04',5000.00,30);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A05','Barang A05',10000.00,33);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A06','Barang A06',30000.00,3);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A07','Barang A07',100000.00,14);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A08','Barang A08',15000.00,19);
+insert  into `barang`(`kode_barang`,`nama_barang`,`harga_barang`,`stok_barang`) values ('A09','Deterjen Cair Daia',23000.00,20);
